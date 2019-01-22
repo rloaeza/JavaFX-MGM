ced82243b2aeecc031cb6b95169457a9a6f60bb7
Roberto Loaeza Valerio <roberto.loaeza@gmail.com>
2019-01-22 13:35:36 -0600

    Trabajando

--- src/controlador/RelojChecador.java
+++ src/controlador/RelojChecador.java
@@ -1,13 +1,27 @@
 package controlador;
 
 import com.jfoenix.controls.JFXTextField;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
+import modelo.Funciones;
+
+import java.io.IOException;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.ResourceBundle;
 
 
-public class RelojChecador extends Controlador {
+public class RelojChecador extends Controlador implements Initializable {
 
     @FXML
     private AnchorPane Pane;
@@ -19,8 +33,20 @@ public class RelojChecador extends Controlador {
     private JFXTextField UsuarioEntrada;
 
     @FXML
-    void entrar(ActionEvent event) {
+    void entrar(ActionEvent event) throws IOException {
         System.out.println(UsuarioEntrada.getText());
+        System.out.println(new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(new Date()));
+
+
+
+
+
+        Map<String,Object> paramsAlert = new LinkedHashMap<>();
+        paramsAlert.put("titulo", "Entrada / Salida");
+        paramsAlert.put("texto", "Bienvenido Roberto");
+        paramsAlert.put("vista", "/vista/alert_box.fxml");
+        Funciones.display(paramsAlert, getClass().getResource("/vista/alert_box.fxml"), new AlertBox() );
+
 
 
     }
@@ -28,5 +54,16 @@ public class RelojChecador extends Controlador {
     @Override
     public void init() {
 
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        //Hora.setText(new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(new Date()));
+        Hora.setText(new SimpleDateFormat("HH:mm").format(new Date()));
+        Timeline tl = new Timeline(new KeyFrame(Duration.millis(1000), ae -> {
+            Hora.setText(new SimpleDateFormat("HH:mm").format(new Date()));
+        } ));
+        tl.setCycleCount(Animation.INDEFINITE);
+        tl.play();
     }
 }
