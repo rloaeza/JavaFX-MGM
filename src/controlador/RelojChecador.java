4b571e6042e45ce7adf0affb07b60bb9071f4397
Roberto Loaeza Valerio <roberto.loaeza@gmail.com>
2019-01-22 14:29:11 -0600

    Trabajando

--- src/controlador/RelojChecador.java
+++ src/controlador/RelojChecador.java
@@ -1,5 +1,8 @@
 package controlador;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
@@ -11,6 +14,7 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.util.Duration;
 import modelo.Funciones;
+import modelo.Personal;
 
 import java.io.IOException;
 import java.net.URL;
@@ -30,24 +34,50 @@ public class RelojChecador extends Controlador implements Initializable {
     private Label Hora;
 
     @FXML
-    private JFXTextField UsuarioEntrada;
+    private JFXPasswordField UsuarioEntrada;
 
     @FXML
     void entrar(ActionEvent event) throws IOException {
-        System.out.println(UsuarioEntrada.getText());
-        System.out.println(new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(new Date()));
+
+        Map<String,Object> paramsJSON = new LinkedHashMap<>();
+        paramsJSON.put("Actividad", "Personal: Lista con codigo");
+        paramsJSON.put("idClinica", parametros.get(0).get("idClinica"));
+        paramsJSON.put("codigo", UsuarioEntrada.getText());
+        JsonArray rootArray = Funciones.consultarBD(paramsJSON);
+        Personal persona=null;
+        if(rootArray.get(0).getAsJsonObject().get(Funciones.res).getAsInt()>0) {
+            int t = rootArray.size();
+            for(int i = 1; i< t; i++) {
+                 persona = new Gson().fromJson(rootArray.get(i).getAsJsonObject(), modelo.Personal.class);
+            }
+        }
 
 
 
+        if(persona!=null) {
+            paramsJSON.clear();
+            paramsJSON.put("Actividad", "Reloj: Agregar");
+            paramsJSON.put("idPersonal", persona.getIdPersonal());
+            paramsJSON.put("fecha", new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(new Date()));
+            rootArray = Funciones.consultarBD(paramsJSON);
+
+            Map<String, Object> paramsAlert = new LinkedHashMap<>();
+            paramsAlert.put("titulo", "Entrada / Salida");
+            paramsAlert.put("texto", "Bienvenido " + persona.getApellidos() + ", " + persona.getNombre());
+            paramsAlert.put("vista", "/vista/alert_box.fxml");
+            Funciones.display(paramsAlert, getClass().getResource("/vista/alert_box.fxml"), new AlertBox());
 
 
-        Map<String,Object> paramsAlert = new LinkedHashMap<>();
-        paramsAlert.put("titulo", "Entrada / Salida");
-        paramsAlert.put("texto", "Bienvenido Roberto");
-        paramsAlert.put("vista", "/vista/alert_box.fxml");
-        Funciones.display(paramsAlert, getClass().getResource("/vista/alert_box.fxml"), new AlertBox() );
+        }
+        else {
+            Map<String, Object> paramsAlert = new LinkedHashMap<>();
+            paramsAlert.put("titulo", "Entrada / Salida");
+            paramsAlert.put("texto", "Error en código");
+            paramsAlert.put("vista", "/vista/alert_box.fxml");
+            Funciones.display(paramsAlert, getClass().getResource("/vista/alert_box.fxml"), new AlertBox());
+        }
 
-
+        UsuarioEntrada.setText("");
 
     }
 
