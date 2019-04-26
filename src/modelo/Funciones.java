3ffe23c3a58ccff0715857032670a9a3d698814c
Roberto <roberto.loaeza@gmail.com>
2019-04-26 00:12:55 -0500

    trabajando

--- src/modelo/Funciones.java
+++ src/modelo/Funciones.java
@@ -389,4 +389,84 @@ public class Funciones {
             httpclient.close();
         }
     }
+
+    public static void writeBitmap(byte[] imageBuf, int nWidth, int nHeight,
+                                   String path) throws IOException {
+        java.io.FileOutputStream fos = new java.io.FileOutputStream(path);
+        java.io.DataOutputStream dos = new java.io.DataOutputStream(fos);
+
+        int w = (((nWidth+3)/4)*4);
+        int bfType = 0x424d; // 位图文件类型（0—1字节）
+        int bfSize = 54 + 1024 + w * nHeight;// bmp文件的大小（2—5字节）
+        int bfReserved1 = 0;// 位图文件保留字，必须为0（6-7字节）
+        int bfReserved2 = 0;// 位图文件保留字，必须为0（8-9字节）
+        int bfOffBits = 54 + 1024;// 文件头开始到位图实际数据之间的字节的偏移量（10-13字节）
+
+        dos.writeShort(bfType); // 输入位图文件类型'BM'
+        dos.write(changeByte(bfSize), 0, 4); // 输入位图文件大小
+        dos.write(changeByte(bfReserved1), 0, 2);// 输入位图文件保留字
+        dos.write(changeByte(bfReserved2), 0, 2);// 输入位图文件保留字
+        dos.write(changeByte(bfOffBits), 0, 4);// 输入位图文件偏移量
+
+        int biSize = 40;// 信息头所需的字节数（14-17字节）
+        int biWidth = nWidth;// 位图的宽（18-21字节）
+        int biHeight = nHeight;// 位图的高（22-25字节）
+        int biPlanes = 1; // 目标设备的级别，必须是1（26-27字节）
+        int biBitcount = 8;// 每个像素所需的位数（28-29字节），必须是1位（双色）、4位（16色）、8位（256色）或者24位（真彩色）之一。
+        int biCompression = 0;// 位图压缩类型，必须是0（不压缩）（30-33字节）、1（BI_RLEB压缩类型）或2（BI_RLE4压缩类型）之一。
+        int biSizeImage = w * nHeight;// 实际位图图像的大小，即整个实际绘制的图像大小（34-37字节）
+        int biXPelsPerMeter = 0;// 位图水平分辨率，每米像素数（38-41字节）这个数是系统默认值
+        int biYPelsPerMeter = 0;// 位图垂直分辨率，每米像素数（42-45字节）这个数是系统默认值
+        int biClrUsed = 0;// 位图实际使用的颜色表中的颜色数（46-49字节），如果为0的话，说明全部使用了
+        int biClrImportant = 0;// 位图显示过程中重要的颜色数(50-53字节)，如果为0的话，说明全部重要
+
+        dos.write(changeByte(biSize), 0, 4);// 输入信息头数据的总字节数
+        dos.write(changeByte(biWidth), 0, 4);// 输入位图的宽
+        dos.write(changeByte(biHeight), 0, 4);// 输入位图的高
+        dos.write(changeByte(biPlanes), 0, 2);// 输入位图的目标设备级别
+        dos.write(changeByte(biBitcount), 0, 2);// 输入每个像素占据的字节数
+        dos.write(changeByte(biCompression), 0, 4);// 输入位图的压缩类型
+        dos.write(changeByte(biSizeImage), 0, 4);// 输入位图的实际大小
+        dos.write(changeByte(biXPelsPerMeter), 0, 4);// 输入位图的水平分辨率
+        dos.write(changeByte(biYPelsPerMeter), 0, 4);// 输入位图的垂直分辨率
+        dos.write(changeByte(biClrUsed), 0, 4);// 输入位图使用的总颜色数
+        dos.write(changeByte(biClrImportant), 0, 4);// 输入位图使用过程中重要的颜色数
+
+        for (int i = 0; i < 256; i++) {
+            dos.writeByte(i);
+            dos.writeByte(i);
+            dos.writeByte(i);
+            dos.writeByte(0);
+        }
+
+        byte[] filter = null;
+        if (w > nWidth)
+        {
+            filter = new byte[w-nWidth];
+        }
+
+        for(int i=0;i<nHeight;i++)
+        {
+            dos.write(imageBuf, (nHeight-1-i)*nWidth, nWidth);
+            if (w > nWidth)
+                dos.write(filter, 0, w-nWidth);
+        }
+        dos.flush();
+        dos.close();
+        fos.close();
+    }
+
+    public static byte[] changeByte(int data) {
+        return intToByteArray(data);
+    }
+    public static byte[] intToByteArray (final int number) {
+        byte[] abyte = new byte[4];
+        // "&" 与（AND），对两个整型操作数中对应位执行布尔代数，两个位都为1时输出1，否则0。
+        abyte[0] = (byte) (0xff & number);
+        // ">>"右移位，若为正数则高位补0，若为负数则高位补1
+        abyte[1] = (byte) ((0xff00 & number) >> 8);
+        abyte[2] = (byte) ((0xff0000 & number) >> 16);
+        abyte[3] = (byte) ((0xff000000 & number) >> 24);
+        return abyte;
+    }
 }
